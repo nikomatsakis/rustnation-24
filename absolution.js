f25d57343f391a643a30f2dc26ef8fc6c4999694
Niko Matsakis <niko@alum.mit.edu>
2024-03-20 05:09:49 -0700

    support custom width

--- absolution.js
+++ absolution.js
@@ -3,6 +3,8 @@ function absolution() {
         console.log(`found element ${JSON.stringify(e.classList)}`);
         var top = NaN;
         var left = NaN;
+        var width = NaN;
+        var height = NaN;
         for (const c of e.classList) {
             if (c.startsWith("top")) {
                 top = Number(c.substring(3));
@@ -10,6 +12,14 @@ function absolution() {
 
             if (c.startsWith("left")) {
                 left = Number(c.substring(4));
+            }
+
+            if (c.startsWith("width")) {
+                width = Number(c.substring(5));
+            }
+
+            if (c.startsWith("height")) {
+                height = Number(c.substring(6));
             }
         }
 
@@ -23,6 +33,16 @@ function absolution() {
         if (left === left) {
             console.log(`found left ${left}`);
             e.style.left = `${left}px`;
+        }
+
+        if (width === width) {
+            console.log(`found width ${width}`);
+            e.style.width = `${width}px`;
+        }
+
+        if (height === height) {
+            console.log(`found height ${height}`);
+            e.style.height = `${height}px`;
         }
     }
 }
